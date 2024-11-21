acae94b6d9f2d0327663ed012ac69dba2ab287a9
Dr Chamyoung 医者 <studio.inbora@gmail.com>
2024-11-20 22:27:57 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,34 +1,22 @@
 class Solution {
-public:
-    int minDistance(string word1, string word2) {
-        int m = word1.length();
-        int n = word2.length();
+ public:
+  int longestPalindromeSubseq(string s) {
+    const int n = s.length();
+    vector<vector<int>> mem(n, vector<int>(n));
+    return lps(s, 0, n - 1, mem);
+  }
 
-        // Create a 2D DP table to store the minimum number of operations
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
-
-        // Base cases
-        for (int i = 0; i <= m; i++) {
-            dp[i][0] = i; // Deleting all characters from word1
-        }
-
-        for (int j = 0; j <= n; j++) {
-            dp[0][j] = j; // Inserting all characters to word1
-        }
-
-        // Fill the DP table
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (word1[i - 1] == word2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1]; // No operation needed if characters are same
-                } else {
-                    dp[i][j] = min({dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + 1});
-                    // Min of: delete, insert, or replace
-                }
-            }
-        }
-
-        // The answer is in the bottom-right corner of the DP table
-        return dp[m][n];
-    }
+ private:
+  // Returns the length of LPS(s[i..j]).
+  int lps(const string& s, int i, int j, vector<vector<int>>& mem) {
+    if (i > j)
+      return 0;
+    if (i == j)
+      return 1;
+    if (mem[i][j] > 0)
+      return mem[i][j];
+    if (s[i] == s[j])
+      return mem[i][j] = 2 + lps(s, i + 1, j - 1, mem);
+    return mem[i][j] = max(lps(s, i + 1, j, mem), lps(s, i, j - 1, mem));
+  }
 };
